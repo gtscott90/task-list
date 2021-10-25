21c6912fa2ac201f3bfbb8a2cd83560431d0187a
Greg Scott <gtscott90@gmail.com>
2021-10-25 15:42:55 -0400

    added the FaTimes X icon on the task component

--- task-list/src/components/Task.js
+++ task-list/src/components/Task.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import { FaTimes } from "react-icons/fa";
 
 export default function Task({ task }) {
-    return (
-        <div className="task">
-            <h3>{task.text}</h3>
-            <p>{task.day}</p>
-            <p>{task.time}</p>
-        </div>
-    )
+  return (
+    <div className="task">
+      <h3>
+        {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }}/>
+      </h3>
+      <p>{task.day}</p>
+      <p>{task.time}</p>
+    </div>
+  );
 }
