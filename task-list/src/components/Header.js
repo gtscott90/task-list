8f04937de326d7d77b190ebd6a41655158f06b04
Greg Scott <gtscott90@gmail.com>
2021-11-08 14:12:09 -0500

    recommit of header changes with add task button

--- task-list/src/components/Header.js
+++ task-list/src/components/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import Button from "./Button";
 
 export default function Header({ title, onAdd, showAdd }) {
-
   return (
     <div className="header">
       <h1>{title}</h1>
-      <Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add Task"} onClick={onAdd} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add Task"}
+        onClick={onAdd}
+      />
     </div>
   );
 }
